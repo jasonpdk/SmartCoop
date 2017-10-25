45ca79913e1d38d12834fc24f0778228aa8626d5
jasonpdk <jason.keane@rocketmail.com>
2017-10-25 21:56:39 +0100

    Thingspeak, JSON

--- thingSpeak.ino
+++ thingSpeak.ino
@@ -3,23 +3,34 @@
  * Jason Keane
 */
 
-void uploadToThingSpeak(float value)
+void uploadToThingSpeak(float value, int field)
 {
   char thingSpeakServer[] = "api.thingspeak.com";
   String APIKey = "5LA2APGVOBKKBW4C";
 
-  if (client.connect(thingSpeakServer, 80))
+  if (!client.connected())
   {
-    // Make a HTTP request:
-    client.print("GET /update?api_key=");
-    client.print(APIKey);
-    client.print("&field1=");
-    client.print(value);
-    client.print("HTTP/1.1");
-    client.println();
+    if (client.connect(thingSpeakServer, 80))
+    {
+      // Make a HTTP request:
+      client.print("GET /update?api_key=");
+      client.print(APIKey);
+      client.print("&field");
+      client.print(field);
+      client.print("=");
+      client.print(value);
+      client.print("HTTP/1.1");
+      client.println();
 
-    client.println("Host: api.thingspeak.com");
-    client.println("Connection: close");
-    client.println();
+      client.println("Host: api.thingspeak.com");
+      client.println("Connection: close");
+      client.println();
+    }
+    else
+    {
+      Serial.println("FAILED THINGSPEAK");
+    }
+
+    client.stop();
   }
 }
