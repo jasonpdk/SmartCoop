6ff76243721343716dd3c503025478f2538b9325
jasonpdk <jason.keane@rocketmail.com>
2017-11-05 20:05:27 +0000

    IP Change

--- websiteFiles/javascript.js
+++ websiteFiles/javascript.js
@@ -1,27 +1,29 @@
+var galileoIP = "192.168.41.102";
+
 function ledOn()
 {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', "//192.168.41.103?LEDOn", true);
+  xhr.open('GET', "//" + galileoIP + "?LEDOn", true);
   xhr.send();
 }
 
 function ledOff()
 {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', "//192.168.41.103?LEDOff", true);
+  xhr.open('GET', "//" + galileoIP + "?LEDOff", true);
   xhr.send();
 }
 
 function openDoor()
 {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', "//192.168.41.103?openDoor", true);
+  xhr.open('GET', "//" + galileoIP + "?openDoor", true);
   xhr.send();
 }
 
 function closeDoor()
 {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', "//192.168.41.103?closeDoor", true);
+  xhr.open('GET', "//" + galileoIP + "?closeDoor", true);
   xhr.send();
 }
