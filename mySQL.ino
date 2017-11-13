60f410f3a6dc93d37a4221b8fa8ad6d9320d28b8
jasonpdk <jason.keane@rocketmail.com>
2017-11-13 08:14:40 +0000

    Style

--- mySQL.ino
+++ mySQL.ino
@@ -3,10 +3,8 @@ char mySQLServer[] = "192.168.41.110";
 void mySQLAdd()
 {
 
-  if (!client.connected())
-  {
-    if (client.connect(mySQLServer, 80))
-    {
+  if (!client.connected()) {
+    if (client.connect(mySQLServer, 80)) {
       client.print("GET /smartcoop/add.php?outsideTemp=");
       client.print(temperature);
       client.print("&insideTemp=");
@@ -27,10 +25,8 @@ void mySQLAdd()
 
 void mySQLAddTimes() // this needs testing
 {
-  if (!client.connected())
-  {
-    if (client.connect(mySQLServer, 80))
-    {
+  if (!client.connected()) {
+    if (client.connect(mySQLServer, 80)) {
       client.print("GET /smartcoop/addTimes.php?sunrise=");
       client.print(sunrise);
       client.print("&sunset=");
