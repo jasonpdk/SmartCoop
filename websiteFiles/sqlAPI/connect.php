bc5a59ca436e7230c38027adde132d9cc31de421
jasonpdk <jason.keane@rocketmail.com>
2017-11-13 08:15:47 +0000

    PHP SQL Connection

---
+++ websiteFiles/sqlAPI/connect.php
@@ -0,0 +1,14 @@
+<?php
+  $servername = "localhost";
+  $username = "SmartCoopUser";
+  $password = "password";
+  $dbname = "SmartCoop";
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  if ($conn->connect_error)
+  {
+      die("Connection failed: " . $conn->connect_error);
+  }
+
+?>
