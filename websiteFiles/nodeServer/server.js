0f979f9a0dddee3357ce760bcd7f8b9898f0b612
jasonpdk <jason.keane@rocketmail.com>
2017-11-28 10:51:25 +0000

    IP change

--- websiteFiles/nodeServer/server.js
+++ websiteFiles/nodeServer/server.js
@@ -19,44 +19,44 @@ getValues();
 app.get('/', function(req, res) {
   getValues();
 
-	res.render('pages/index', {
-		insideTemp: insideTemp,
-		outsideTemp: outsideTemp,
-		humidity: humidity,
-		sunrise: sunrise,
-		sunset: sunset
-	});
+  res.render('pages/index', {
+    insideTemp: insideTemp,
+    outsideTemp: outsideTemp,
+    humidity: humidity,
+    sunrise: sunrise,
+    sunset: sunset
+  });
 });
 
 // graphs page
-app.get ('/graphs', function(req, res) {
-	res.render('pages/graphs');
+app.get('/graphs', function(req, res) {
+  res.render('pages/graphs');
 });
 
 // camera page
-app.get ('/camera', function(req, res) {
-	res.render('pages/camera');
+app.get('/camera', function(req, res) {
+  res.render('pages/camera');
 });
 
 app.listen(80);
 console.log('The server is on port 80');
 
 
-function getValues()
-{
-  url = "http://192.168.41.110/smartcoop/getLatest.php";
+function getValues() {
+  url = "http://192.168.41.112/smartcoop/getLatest.php";
 
   request({
-    url: url,
-    json: true },
+      url: url,
+      json: true
+    },
 
-    function (error, response, body) {
-    if (response.statusCode == 200) {
-      outsideTemp = body.readings.temperature;
-      insideTemp = body.reading.insideTemperature;
-      humidity = body.readings.humidity;
-      sunrise = body.times.sunrise;
-      sunset = body.times.sunset;
-    }
-  })
+    function(error, response, body) {
+      if (response.statusCode == 200) {
+        outsideTemp = body.readings.temperature;
+        insideTemp = body.reading.insideTemperature;
+        humidity = body.readings.humidity;
+        sunrise = body.times.sunrise;
+        sunset = body.times.sunset;
+      }
+    })
 }
