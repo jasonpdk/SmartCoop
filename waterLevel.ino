4f02ef488b707235845c451d15f88a978a772d7d
jasonpdk <jason.keane@rocketmail.com>
2018-02-06 17:19:39 +0000

    Cleaning

--- waterLevel.ino
+++ waterLevel.ino
@@ -2,10 +2,8 @@
 void checkWaterSensor()
 {
 	if (digitalRead(waterLevelSensor) == LOW) {
-		Serial.println("LOW");
 		digitalWrite(solenoidValve, LOW);
 	} else {
-		Serial.println("HIGH");
 		digitalWrite(solenoidValve, HIGH);
 	}
 }
